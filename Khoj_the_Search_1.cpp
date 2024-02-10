e4cb87b74ba5ec0bb047ae8c22cc5113e5fcf362
Mahfuz <sayhello.mahfuz@gmail.com>
2024-02-11 02:38:31 +0600

    add new solve

---
+++ Khoj_the_Search_1.cpp
@@ -0,0 +1,35 @@
+/// In the name of ALLAH
+
+  /*----------------------------*/
+ /*          Mahfuz            */
+/*----------------------------*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define FASTER ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define MOD 1000000007
+#define all(a) (a).begin(),(a).end()
+using namespace std;
+
+void solve() {
+    string str1, str2; cin >> str1 >> str2;
+
+    int foundAt = str1.find(str2);
+
+    cout << foundAt << "\n";
+}
+int main() {
+    FASTER
+
+    int tt = 1;
+    cin >> tt;
+    // int i = 1;
+    while(tt--){
+        // cout << "Case " << i << ": ";
+        solve();
+        // i++;
+    }
+    return 0;
+}
+
+/// Alhamdulilla for everything
